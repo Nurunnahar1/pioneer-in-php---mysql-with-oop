5d9c90bcae82431ef5b04fb0fc459bec310f5a89
Nurunnahar <nurunnahar9081@gmail.com>
2024-09-06 23:10:35 +0600

    get post request methods

---
+++ get-post-request-methods/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GET POST REQUEST Methods</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-gray-200 flex just">
+    <div class="login">
+        <form action="">
+            <div class="">
+                <label for="name">Name</label>
+                <input type="text" name="name">
+            </div>
+            <div class="">
+                <label for="email">Email</label>
+                <input type="text" name="email">
+            </div>
+            <div class="">
+                <label for="phone">Phone</label>
+                <input type="text" name="phone">
+            </div>
+
+        </form>
+    </div>
+</body>
+
+</html>
